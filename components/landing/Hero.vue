da035ba9de16c7dfd58c6c1409ecfb00cd33c4e9
safayet <work.jamil4@gmail.com>
2025-02-11 17:56:21 +0600

    first commit

---
+++ components/landing/Hero.vue
@@ -0,0 +1,42 @@
+<template>
+  <main class="grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-8">
+    <div class="p-24 md:order-1 hidden md:block">
+      <img
+        class="rounded-full transition-transform transform hover:scale-125"
+        src="~/assets/img/hero.jpg"
+        alt="Starship starts the engine"
+        loading="eager"
+        format="avif"
+        width="512"
+        height="512"
+      />
+    </div>
+
+    <div>
+      <h1 class="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight">
+        Security At Your FingerTips
+      </h1>
+      <p class="text-lg mt-4 text-slate-600 max-w-xl">
+        Roar VPN is a product for personal, public devices &
+        Enterprise Level VPN.<wbr /> Built with Security & High Availability. You can quickly
+        launch you VPN Business or Secure your devices anytime anywhere.
+      </p>
+      <div class="mt-6 flex flex-col sm:flex-row gap-3">
+        <LandingLink
+          href="https://github.com/Gr33nW33n/nuxtship-template"
+          target="_blank"
+          rel="noopener"
+          >Get Started</LandingLink
+        >
+        <LandingLink
+          size="lg"
+          styleName="outline"
+          rel="noopener"
+          href="https://github.com/Gr33nW33n/nuxtship-template"
+          target="_blank"
+          >View Repo</LandingLink
+        >
+      </div>
+    </div>
+  </main>
+</template>
