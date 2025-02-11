9875079be404f57325f4c3e38d1f7cb9afb38eed
Safayet Jamil <work.jamil4@gmail.com>
2025-02-12 00:29:34 +0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,98 @@
 definePageMeta({
   layout: "landing",
 });
+const pricing = [
+  {
+    name: "Personal",
+    price: "Free",
+    popular: false,
+    features: [
+      "Lifetime free",
+      "Up to 3 users",
+      "Unlimited Pages",
+      "Nuxt Sub domain",
+      "Basic Integrations",
+      "Community Support",
+    ],
+    button: {
+      text: "Get Started",
+      link: "/",
+    },
+  },
+  {
+    name: "Startup",
+    price: {
+      monthly: "$19",
+      annual: "$16",
+      discount: "10%",
+      original: "$24",
+    },
+    popular: true,
+    features: [
+      "All Free Features",
+      "Up to 20 users",
+      "20 Custom domains",
+      "Unlimited Collaborators",
+      "Advanced Integrations",
+      "Priority Support",
+    ],
+    button: {
+      text: "Get Started",
+      link: "#",
+    },
+  },
+  {
+    name: "Enterprise",
+    price: "Custom",
+    popular: false,
+    features: [
+      "All Pro Features",
+      "Unlimited Custom domains",
+      "99.99% Uptime SLA",
+      "SAML & SSO Integration",
+      "Dedicated Account Manager",
+      "24/7 Phone Support",
+    ],
+    button: {
+      text: "Contact us",
+      link: "/contact",
+    },
+  },
+];
+const TeamImg1 = "https://source.unsplash.com/IF9TK5Uy-KI";
+const TeamImg2 = "https://source.unsplash.com/iEEBWgY_6lA";
+const TeamImg3 = "https://source.unsplash.com/ZHvM3XIOHoE";
+
+const team = [
+  {
+    name: "Janette Lynch",
+    title: "Senior Director",
+    avatar: {
+      src: TeamImg1,
+      width: 480,
+      height: 560,
+    },
+  },
+  {
+    name: "Marcell Ziemann",
+    title: "Principal Strategist",
+    avatar: {
+      src: TeamImg2,
+      width: 580,
+      height: 580,
+    },
+  },
+  {
+    name: "Robert Palmer",
+    title: "Marketing Engineer",
+    avatar: {
+      src: TeamImg3,
+      width: 580,
+      height: 580,
+    },
+  },
+];
+
 </script>
 
 <template>
@@ -10,5 +102,111 @@ definePageMeta({
     <LandingFeatures></LandingFeatures>
     <LandingLogos></LandingLogos>
     <LandingCta></LandingCta>
+  </LandingContainer>
+    <LandingContainer>
+    <LandingSectionhead>
+      <template v-slot:title>Pricing</template>
+      <template v-slot:desc
+        >Simple & Predictable pricing. No Surprises.</template
+      >
+    </LandingSectionhead>
+
+    <div class="grid md:grid-cols-3 gap-10 mx-auto max-w-screen-lg mt-12">
+      <LandingPricing v-for="item of pricing" :plan="item" />
+    </div>
+  </LandingContainer>
+  <LandingContainer>
+    <LandingSectionhead>
+      <template v-slot:title>About</template>
+      <template v-slot:desc>We are a small passionate team.</template>
+    </LandingSectionhead>
+
+    <div class="flex flex-col gap-3 mx-auto max-w-4xl mt-16">
+      <h2 class="font-bold text-3xl text-gray-800">
+        Empowering the world with Nuxt.
+      </h2>
+      <p class="text-lg leading-relaxed text-slate-500">
+        We're a multi-cultural team from around the world! We come from diverse
+        backgrounds, bringing different personalities, experiences and skills to
+        the job. This is what makes our team so special.
+      </p>
+    </div>
+    <div class="grid md:grid-cols-3 gap-10 mx-auto max-w-4xl mt-12">
+      <div v-for="item of team" class="group">
+        <div class="w-full aspect-square">
+          <img
+            :src="item.avatar.src"
+            :width="item.avatar.width"
+            :height="item.avatar.height"
+            format="avif"
+            alt="Team"
+            class="w-full h-full object-cover rounded transition group-hover:-translate-y-1 group-hover:shadow-xl"
+          />
+        </div>
+
+        <div class="mt-4 text-center">
+          <h2 class="text-lg text-gray-800">{{ item.name }}</h2>
+          <h3 class="text-sm text-slate-500">{{ item.title }}</h3>
+        </div>
+      </div>
+      <!-- {
+        team.map((item) => (
+          <div class="group">
+            <div class="w-full aspect-square">
+              <Image
+                {...item.avatar}
+                format="avif"
+                alt="Team"
+                class="w-full h-full object-cover rounded transition  group-hover:-translate-y-1 group-hover:shadow-xl"
+              />
+            </div>
+
+            <div class="mt-4 text-center">
+              <h2 class="text-lg text-gray-800"> {item.name}</h2>
+              <h3 class="text-sm text-slate-500"> {item.title}</h3>
+            </div>
+          </div>
+        ))
+      } -->
+    </div>
+  </LandingContainer>
+    <LandingContainer>
+    <LandingSectionhead>
+      <template v-slot:title>Contact</template>
+      <template v-slot:desc>I am here to help.</template>
+    </LandingSectionhead>
+
+    <div class="grid md:grid-cols-2 gap-10 mx-auto max-w-4xl mt-16">
+      <div>
+        <h2 class="font-medium text-2xl text-gray-800">
+          Contact Nuxtship Creator
+        </h2>
+        <p class="text-lg leading-relaxed text-slate-500 mt-3">
+          Have something to say? I am here to help. Fill up the form or send
+          email.
+        </p>
+        <div class="mt-5">
+          <div class="flex items-center mt-2 space-x-2 text-gray-600">
+            <Icon class="text-gray-400 w-4 h-4" name="uil:map-marker" />
+            <span>1010 Vienna, Austria</span>
+          </div>
+          <div class="flex items-center mt-2 space-x-2 text-gray-600">
+            <Icon class="text-gray-400 w-4 h-4" name="uil:envelope" /><a
+              href="mailto:jakobaichmayr@gmail.com"
+              >jakobaichmayr@gmail.com</a
+            >
+          </div>
+          <!-- <div class="flex items-center mt-2 space-x-2 text-gray-600">
+            <Icon class="text-gray-400 w-4 h-4" name="uil:phone" /><a
+              href="tel:+1 (234) 5678 999"
+              >+1 (234) 5678 999</a
+            >
+          </div> -->
+        </div>
+      </div>
+      <div>
+        <LandingContactform />
+      </div>
+    </div>
   </LandingContainer>
 </template>
